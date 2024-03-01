04b056f8abd8c93184179190048120f0eb32d48f
akbarshamjijr <akbar_shamji@brown.edu>
2024-03-01 00:14:16 -0500

    feat - mocked test initial

--- mock/tests/e2e/App.spec.ts
+++ mock/tests/e2e/App.spec.ts
@@ -69,6 +69,18 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Submit' }).click();
 });
 
-test('after I click the button, my command gets pushed', async ({ page }) => {
-  // TODO: Fill this in to test your button push functionality!
+
+test('testing mock value', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Login').click();
+  await expect(page.getByPlaceholder('Enter command here!')).toBeEmpty();
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('loadcsv');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('#root')).toContainText('CSV loaded!');
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('viewcsv');
+  await expect(page.getByPlaceholder('Enter command here!')).toHaveValue('viewcsv');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('#root')).toContainText('[["ID","Name","Occupation","Age"],["1","John Doe","Software Engineer","30"],["2","Jane Smith","Data Scientist","28"],["3","Alice Johnson","Product Manager","35"],["4","Bob Brown","Graphic Designer","26"],["5","Charlie Davis","Marketing Specialist","32"]]');
 });
